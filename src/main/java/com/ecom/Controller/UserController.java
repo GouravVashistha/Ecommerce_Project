c5ba9c34fe27141c976aae588813eea07172835a
GOURAV <gvashistha4@gmail.com>
2026-05-20 23:21:43 +0530

    user service

---
+++ src/main/java/com/ecom/Controller/UserController.java
@@ -0,0 +1,13 @@
+package com.ecom.Controller;
+
+import com.ecom.Entity.User;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    public List<User> getAllUsers(){
+        return null;
+    }
+}
---
+++ src/main/java/com/ecom/Entity/User.java
@@ -0,0 +1,7 @@
+package com.ecom.Entity;
+
+public class User {
+    private  Long id;
+    private String firstName;
+    private String lastName;
+}
